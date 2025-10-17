96b9a851220bf99b79955b43164d4f80c1673554
Pratik R Chaudhari <pratik.chaudhari0410@gmail.com>
2025-10-16 23:56:46 -0700

    Add files via upload

---
+++ GCDStrings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        var m = str1.length();
+        var n = str2.length();
+        if((str1 + str2).equals(str2 + str1)){
+            if(m>n){
+                while(n!=0){
+                    int temp = n;
+                    n = m % n;
+                    m = temp;
+                }
+               return str1.substring(0,m); 
+            }else{
+                 while(m!=0){
+                    int temp = m;
+                    m = n % m ;
+                    n = temp;
+                }
+                return str1.substring(0,n);
+            }
+        }else{
+            return "";
+        }
+        //return str1.substring(0,m);
+    }
+}
