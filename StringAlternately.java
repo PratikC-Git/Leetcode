584be22f70b68f6d7c34eec77402d33df04c5329
Pratik R Chaudhari <pratik.chaudhari0410@gmail.com>
2025-10-16 23:54:37 -0700

    Add files via upload

---
+++ StringAlternately.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder res = new StringBuilder();
+        int w1,w2;
+        w1=w2=0;
+        while(w1<word1.length() && w2<word2.length()){
+        res.append(word1.charAt(w1));
+        res.append(word2.charAt(w2));
+        w1++; 
+        w2++;
+        }
+        if(w1<word1.length()){
+            res.append(word1.substring(w1));
+        }
+        else if(w2<word2.length()){
+            res.append(word2.substring(w2));
+        }
+        return res.toString();
+    }
+}
