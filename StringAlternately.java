c787558ef610dffbf62fc1fe34093eb623da26a0
Pratik R Chaudhari <pratik.chaudhari0410@gmail.com>
2025-10-17 23:52:31 -0700

    Update StringAlternately.java
    Fix formatting for variable initialization.

--- StringAlternately.java
+++ StringAlternately.java
@@ -2,7 +2,7 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder res = new StringBuilder();
         int w1,w2;
-        w1=w2=0;
+        w1 = w2 = 0;
         while(w1<word1.length() && w2<word2.length()){
         res.append(word1.charAt(w1));
         res.append(word2.charAt(w2));
