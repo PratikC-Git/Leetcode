20d68bd4fbc111be6d8f517114edef66b8382e97
Pratik R Chaudhari <pratik.chaudhari0410@gmail.com>
2025-10-17 23:53:27 -0700

    Update GCDStrings.java
    Refactor while loop conditions for clarity.

--- GCDStrings.java
+++ GCDStrings.java
@@ -4,14 +4,14 @@ class Solution {
         var n = str2.length();
         if((str1 + str2).equals(str2 + str1)){
             if(m>n){
-                while(n!=0){
+                while(n != 0){
                     int temp = n;
                     n = m % n;
                     m = temp;
                 }
                return str1.substring(0,m); 
             }else{
-                 while(m!=0){
+                 while(m != 0){
                     int temp = m;
                     m = n % m ;
                     n = temp;
