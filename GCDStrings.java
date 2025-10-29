cc792bf17dc2af34dba7a82669a102910751efbb
Pratik R Chaudhari <pratik.chaudhari0410@gmail.com>
2025-10-29 00:44:09 -0700

    Update GCDStrings.java

--- GCDStrings.java
+++ GCDStrings.java
@@ -2,7 +2,7 @@ class Solution {
     public String gcdOfStrings(String str1, String str2){
         var m = str1.length();
         var n = str2.length();
-        if((str1 + str2).equals(str2+str1)) {
+        if((str1 + str2).equals(str2+str1)){
             if(m>n){
                 while(n != 0) {
                     int temp = n;
